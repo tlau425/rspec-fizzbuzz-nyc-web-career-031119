a458c93c0936f04868989b5bbb0be31394643f24
Tommy Lau <tlau425@yahoo.com>
2019-02-22 03:54:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int / 3
+  if int % 3 = 0
     return "Fizz"
   
-  elsif int / 5
+  elsif int % 5 = 0
     return "Buzz"
   
-  elsif int / 3 && int / 5
+  elsif int % 3 = 0 && int % 5 = 0
     return "FizzBuzz"
   
   else
