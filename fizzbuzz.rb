a246c0bd4697aa70823dc1a6cec0b000e5d48afe
Tommy Lau <tlau425@yahoo.com>
2019-02-22 03:56:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,14 +5,11 @@ def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
   
-  if int % 3 == 0
+  elsif int % 3 == 0
     return "Fizz"
   
   elsif int % 5== 0
     return "Buzz"
-  
-  elsif int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
   
   else
     return "nil"
