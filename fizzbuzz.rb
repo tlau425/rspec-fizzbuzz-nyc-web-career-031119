16935cba8acdc88ef68a1574ef7ad523702325c9
Tommy Lau <tlau425@yahoo.com>
2019-02-22 03:52:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(int)
+  if int / 3
+    return "Fizz"
   
+  elsif int / 5
+    return "Buzz"
+  
+  elsif int / 3 && int / 5
+    return "FizzBuzz"
+  
+  else
+    return "nil"
+  end
 end
